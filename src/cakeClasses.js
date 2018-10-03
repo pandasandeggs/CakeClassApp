67529f1e080d2df9ff20b47744e1cbef8f678f13
pandasandeggs <theteatreehouse@gmail.com>
2018-10-03 17:51:00 -0500

    first commit

---
+++ src/cakeClasses.js
@@ -0,0 +1,46 @@
+const cakeClasses = [
+  {
+    class: "White House Cake"
+    instructor: 'Colette Peters',
+    cakeType: 'Special Event Cakes',
+    seenOnTv: true,
+    cake_img: "http://www.colettescakes.com/photos/specialEvents/005-inauguration_sm.jpg",
+  },
+  {
+    class: "Rubix Cube Cake"
+    instructor: 'Duff Goldman',
+    cakeType: 'Custom Cakes',
+    seenOnTv: true,
+    cake_img: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/3/17/0/DT_rubiks-cubs_s3x4.jpg.rend.hgtvcom.966.1288.suffix/1426615684506.jpeg",
+  },
+  {
+    class: "Gumpaste Flower Class"
+    instructor: 'Nicholas Lodge',
+    cakeType: 'Custom Cakes',
+    seenOnTv: false,
+    cake_img: "https://www.nicholaslodge.com/images/igallery/resized/101-200/IMG_1338_2113966937_O-128-1200-800-80-wm-right_bottom-100-nlimagewatermarkpng.jpg",
+  },
+  {
+    class: "Baseball Stadium Cake"
+    instructor: 'Buddy Valastro',
+    cakeType: 'Special Event Cakes',
+    seenOnTv: true,
+    cake_img: "https://images.firstwefeast.com/complex/image/upload/c_limit,f_auto,fl_lossy,q_auto,w_1100/piml82cpx2omqqyruqx2",
+  },
+  {
+    class: "Armadillo Cake"
+    instructor: 'Natalie and Dave Sideserf',
+    cakeType: 'Custom Cakes',
+    seenOnTv: true,
+    cake_img: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2017/7/13/0/TB0201_Armadillo.jpg.rend.hgtvcom.966.725.suffix/1500247922829.jpeg",
+  },
+  {
+    class: "Paris MadHatter Cake"
+    instructor: 'Michelle Smith',
+    cakeType: 'Custom Cakes',
+    seenOnTv: false,
+    cake_img: "https://cakecraze.com.au/wp-content/uploads/2014/02/PC-paris-madhatter-by-me-AM.jpg",
+  },
+];
+
+export default cakeClasses;
